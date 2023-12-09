853e5ad749b81637a67bdc19298353855be942f6
Muhammad Nopran <noprantor@gmail.com>
2023-12-09 14:57:48 +0000

    upload_files

--- analisis_review_produk.py
+++ analisis_review_produk.py
@@ -1,16 +1,59 @@
 import streamlit as st
+import pandas as pd
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
+## Load Data
+def load_data():
+    data = pd.read_csv("20191002-reviews.csv")
+    return data
+
+def plot_word_cloud(data):
+    text = " ".join(str(review) for review in data.reviewContent)
+    wordcloud = WordCloud(background_color='white').generate(text)
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+    st.pyplot(plt.gcf())
+
+def show_desriptive_analysis(data):
+    st.write('### Analsisis deskriptif')
+    st.write(data.describe())
+    plot_word_cloud(data)
 
 def main() :
-    st.title("Klasifikasi Sentimen Review Produk (Studi Kasu Lazada Dataset)")
+
+    st.title("Klasifikasi Sentimen Review Produk (Studi Kasus Lazada Dataset)")
     menu_options = {
      "Home" :":house:",
      "Dataset":":book:",
      "Model":":gear:",
-     "Analisis Deskriptif ":":bar_charts:",
-     "About":":ballon:"
+     "Analisis Deskriptif":":bar_chart:",
+     "About":":balloon:"
      }
 
-
     st.sidebar.title("Menu")
-    choice = st.sidebar.radio("",list(menu_options,keys()),format_func=lambda x: f"{menu_options[x]}{x}")
+    choice = st.sidebar.radio("", list(menu_options.keys()), format_func=lambda x: f"{menu_options[x]} {x}")
+
+    if choice == "Home":
+        st.subheader("Home")
+        st.write("Selamat datang di portofolio analisis review produk (dataset lazada)")
+    elif choice =="Dataset":
+        st.subheader("Dataset")
+        st.write("Dataset yang digunakan")
+        data = load_data()
+        st.write(data.head(50))
+    elif choice =="Model":
+        st.subheader("Model")
+        st.write("Model yang digunakan")
+    elif choice =="Analisis Deskriptif":
+        st.subheader("Analisis Deskriptif")
+        st.write("Analisis Deskriptif ")
+        data =load_data()
+        show_desriptive_analysis(data)
+    elif choice =="About":
+        st.subheader("About")
+        st.write("About ")  
+
+
+if __name__ == "__main__":
+    main()
